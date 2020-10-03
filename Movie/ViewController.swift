594620065a4b549c1f85ecabb7d461f585b820f3
Wei-Lun Su <allensu212@gmail.com>
2020-10-02 20:49:11 -0400

    Initial Commit

---
+++ Movie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie
+//
+//  Created by Wei-Lun Su on 10/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
